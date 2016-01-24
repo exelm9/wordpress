edece1f9e0021c967c4637ba2875d348a402d824
jobcrackerbah <erik@critiqueit.com>
2016-01-24 12:47:23 -0800

    Adds wordpress menu option
    Adds to wp admin sidebar under Appearance.

--- wp-content/themes/testTheme/functions.php
+++ wp-content/themes/testTheme/functions.php
@@ -1,9 +1,19 @@
 <?php
 
-// declare function and run in add action
+// Holds wordpress custom scripts to be included.  Declare function then run in add action
 function test_enqueue() {
-	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/test.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('customStyleId', get_template_directory_uri() . '/css/test.css', array(), '1.0.0', 'all');
+	wp_enqueue_script('customJsId', get_template_directory_uri() . '/js/test.js', array(), '1.0.0', true);
 }
 
-// hook that gives connection wordpress execution process to a custom function
+// hook that gives connection wordpress execution process to a custom function.  Gets triggered by wp_head();
 add_action('wp_enqueue_scripts', 'test_enqueue');
+
+// activitate specific theme support with a hook.  Easier to use functions because easy to keep track
+function test_theme_setup(){
+	add_theme_support('menus');
+};
+
+// use init because adding more functions to test them setup
+add_action('init', 'test_theme_setup');
+
