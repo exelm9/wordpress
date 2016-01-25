f4d50e2948035d661f428471e9a6a13e19415883
jobcrackerbah <erik@critiqueit.com>
2016-01-24 19:44:42 -0800

    Adds masthead image

--- wp-content/themes/testTheme/header.php
+++ wp-content/themes/testTheme/header.php
@@ -4,9 +4,12 @@
 	<meta charset="utf-8">
 	<title>Test Theme</title>
 	<!-- connects wordpress hooks -->
-	<?php wp_head(); ?>
+		<?php wp_head(); ?>
+	
 </head>
 
 <body>
+<header>
 	<!-- grabs first menu available.  If want to preload other menus, have to understand paramters passed -->
 	<?php wp_nav_menu(array('theme_location'=>'main')); ?>
+</header>
