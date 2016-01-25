626ffa91dd4a76fbb9b4b1710c80d99e7c28e2a1
jobcrackerbah <erik@critiqueit.com>
2016-01-24 19:44:59 -0800

    Adds connection to posts
    posts are located in db

--- wp-content/themes/testTheme/index.php
+++ wp-content/themes/testTheme/index.php
@@ -1,3 +1,17 @@
 <?php get_header(); ?>
-<h1>This the main man file</h1>
+
+	<?php if( have_posts() ):
+
+		while( have_posts() ): the_post(); ?>
+
+			<h3><?php the_title(); ?></h3>
+
+			<p><?php the_content(); ?></p>
+
+		<?php endwhile;
+
+	endif;
+
+	?>
+
 <?php get_footer(); ?>
